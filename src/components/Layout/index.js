9891f00555764bb344f557033c7f4b7220a2cbf7
ABE <abeabeabeabe444@gmail.com>
2023-09-17 20:50:32 +0700

    ✨ mui

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -7,6 +7,11 @@ import Image from "next/image";
 import whatsapp from "./../../../public/assets/logo/whatsapp.png";
 import Sidebar from "./Sidebar";
 import LiveChatComponent from "../common/LiveChat";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 export default function Layout(props) {
   const { children } = props;
