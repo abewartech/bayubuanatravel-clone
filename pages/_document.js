1b61563df8fe7fe8f370bc9c7284c044b436bb9b
ABE <abeabeabeabe444@gmail.com>
2023-09-24 09:41:15 +0700

    💄 chat box

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,12 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"
+        />
       </Head>
       <body>
         <Main />
--- src/components/common/LiveChat/index.js
+++ src/components/common/LiveChat/index.js
@@ -1,7 +1,12 @@
 import Image from "next/image";
 import livechat from "../../../../public/assets/logo/livechat.png";
 import styles from "./LiveChat.module.scss";
+import Button from "@mui/material/Button";
 import { useState, useEffect } from "react";
+import ClearIcon from "@mui/icons-material/Clear";
+import { IconButton, TextField } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
+import Box from "@mui/material/Box";
 export default function LiveChatComponent(props) {
   const [messages, setMessages] = useState([]);
   const [inputMessage, setInputMessage] = useState("");
@@ -35,42 +40,63 @@ export default function LiveChatComponent(props) {
           {/* Chat Box Header */}
           <div className={styles.chat_box_header}>
             ChatBot
-            <span className={styles.chat_box_toggle} onClick={toggleChat}>
-              <i className="material-icons">close</i>
-            </span>
+            <IconButton
+              aria-label="delete"
+              color="success"
+              className={styles.chat_box_toggle}
+              onClick={toggleChat}
+              style={{ color: "white" }}
+            >
+              <ClearIcon />
+            </IconButton>
           </div>
 
           {/* Chat Box Body */}
           <div className={styles.chat_box_body}>
-            <div className={styles.chat_box_overlay}></div>
-            <div className="chat-logs">
-              {messages.map((message, index) => (
-                <div key={index} className={`chat-msg ${message.type}`}>
-                  <span className="msg-avatar">
-                    <img
-                      src="https://image.crisp.im/avatar/operator/196af8cc-f6ad-4ef7-afd1-c45d5231387c/240/?1483361727745"
-                      alt="Avatar"
-                    />
-                  </span>
-                  <div className="cm-msg-text">{message.text}</div>
-                </div>
-              ))}
-            </div>
+            <Box sx={{ p: 2, height: 300, }}>
+              <div className={styles.chat_box_overlay}></div>
+              <div className="chat-logs">
+                {messages.map((message, index) => (
+                  <div key={index} className={`chat-msg ${message.type}`}>
+                    <span className="msg-avatar">
+                      <img
+                        src="https://image.crisp.im/avatar/operator/196af8cc-f6ad-4ef7-afd1-c45d5231387c/240/?1483361727745"
+                        alt="Avatar"
+                      />
+                    </span>
+                    <div className="cm-msg-text">{message.text}</div>
+                  </div>
+                ))}
+              </div>
+            </Box>
           </div>
 
           {/* Chat Box Input */}
           <div className="chat-input">
             <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                id="chat-input"
-                placeholder="Send a message..."
-                value={inputMessage}
-                onChange={(e) => setInputMessage(e.target.value)}
-              />
-              <button type="submit" className="chat-submit" id="chat-submit">
-                <i className="material-icons">send</i>
-              </button>
+              <Grid container spacing={2}>
+                <Grid xs={8}>
+                  <TextField
+                    type="text"
+                    id="chat-input"
+                    value={inputMessage}
+                    onChange={(e) => setInputMessage(e.target.value)}
+                    label="Send a message..."
+                    variant="outlined"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid xs={4}>
+                  <Button
+                    variant="contained"
+                    type="submit"
+                    className="chat-submit"
+                    id="chat-submit"
+                  >
+                    Send
+                  </Button>
+                </Grid>
+              </Grid>
             </form>
           </div>
         </div>
