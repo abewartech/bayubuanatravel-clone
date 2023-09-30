34d5cc0173211c693a2c9ea4bf2dbc3f2ba60e2a
ABE <abeabeabeabe444@gmail.com>
2023-09-30 16:48:40 +0700

    ✨ axios

---
+++ pages/register.js
@@ -0,0 +1,217 @@
+// import { useRouter } from "next/router";
+import Link from "next/link";
+import Layout from "../src/components/Layout";
+import HeaderPage from "../src/components/common/HeaderPage";
+import resort from "./../public/assets/resort.jpg";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import { Box, Button, FormControlLabel, Radio, Typography } from "@mui/material";
+import axios from "axios";
+
+export default function Register() {
+  // const currUrl = useRouter();
+  // const typePage = currUrl.query.type;
+  const breadcrumb = [
+    {
+      name: "Home"
+    },
+    {
+      name: "Register"
+    }
+  ];
+  const initialValues = {
+    address: "",
+    country: "",
+    email: "",
+    full_name: "",
+    gender: "",
+    password: ""
+  };
+  return (
+    <Layout>
+      <HeaderPage
+        title={"Register"}
+        breadcrumb={breadcrumb}
+        background={resort}
+      />
+      <div className="container mb-5">
+        <div className="row">
+          <Formik
+            initialValues={initialValues}
+            validate={(values) => {
+              const errors = {};
+              if (!values.email) {
+                errors.email = "Required";
+              } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+              ) {
+                errors.email = "Invalid email address";
+              }
+              // Add more validation rules as needed
+              return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+              axios.post("http://api.marinarajaampat.id/users/v1/register", values).then(res => {
+                console.log(res);
+              })
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting
+              /* and other goodies */
+            }) => (
+              <Form
+                sx={{ margin: 10, height: "40px" }}
+                noValidate
+                autoComplete="off"
+              >
+                <Typography>
+                  <Box fontSize={16} fontWeight={700} lineHeight="24px">
+                    Full Name
+                  </Box>
+                </Typography>
+                <Field
+                  type="text"
+                  onChange={handleChange}
+                  name="full_name"
+                  placeholder="Your Full Name"
+                />
+                <ErrorMessage name="full_name" component="div" />
+
+                <Typography>
+                  <Box fontSize={16} fontWeight={700} lineHeight="24px">
+                    Address
+                  </Box>
+                </Typography>
+                <Field
+                  type="text"
+                  onChange={handleChange}
+                  name="address"
+                  placeholder="Your Address"
+                />
+                <ErrorMessage name="address" component="div" />
+
+                <Typography>
+                  <Box fontSize={16} fontWeight={700} lineHeight="24px">
+                    Country
+                  </Box>
+                </Typography>
+                <Field
+                  type="text"
+                  onChange={handleChange}
+                  name="country"
+                  placeholder="Your Country"
+                />
+                <ErrorMessage name="country" component="div" />
+
+                <Typography>
+                  <Box fontSize={16} fontWeight={700} lineHeight="24px">
+                    Gender
+                  </Box>
+                </Typography>
+                <div role="group" aria-labelledby="my-radio-group">
+                  <FormControlLabel
+                    control={
+                      <Radio
+                        onChange={handleChange}
+                        name="gender"
+                        value="male"
+                      />
+                    }
+                    label="Male"
+                  />
+                  <FormControlLabel
+                    control={
+                      <Radio
+                        onChange={handleChange}
+                        name="gender"
+                        value="female"
+                      />
+                    }
+                    label="Female"
+                  />
+                </div>
+                <ErrorMessage name="gender" component="div" />
+
+                <Typography>
+                  <Box fontSize={16} fontWeight={700} lineHeight="24px">
+                    Email atau Username
+                  </Box>
+                </Typography>
+
+                <Field
+                  type="text"
+                  onChange={handleChange}
+                  name="email"
+                  placeholder="contoh@versinema.com"
+                />
+                <ErrorMessage name="email" component="div" />
+
+                <Typography>
+                  <Box fontSize={16} fontWeight={700} lineHeight="24px">
+                    Password
+                  </Box>
+                </Typography>
+                <Field
+                  type="password"
+                  onChange={handleChange}
+                  name="password"
+                  autoComplete="on"
+                  placeholder="Password kamu"
+                />
+                <ErrorMessage name="password" component="div" />
+
+                {/* Include other form fields as needed based on your schema */}
+
+                <div
+                  sx={{
+                    display: "flex",
+                    justifyContent: "flex-start",
+                    marginTop: 10
+                  }}
+                >
+                  <a href="forgot-password" color="textPrimary" replace>
+                    <Typography variant="caption" display="block" gutterBottom>
+                      Forget Password ?
+                    </Typography>
+                  </a>
+                </div>
+
+                <div
+                  sx={{
+                    display: "flex",
+                    justifyContent: "flex-start",
+                    marginTop: 10
+                  }}
+                >
+                  <Button type="submit">Register</Button>
+                </div>
+
+                <div
+                  sx={{
+                    display: "flex",
+                    justifyContent: "flex-start",
+                    marginTop: 10
+                  }}
+                >
+                  <Link href="/login" passHref>
+                    <Button component="a">Login</Button>
+                  </Link>
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </div>
+    </Layout>
+  );
+}
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -5,9 +5,33 @@ import logo from "./../../../public/assets/logo/logo.png";
 import mail from "./../../../public/assets/icon/mail.svg";
 import call from "./../../../public/assets/icon/call.svg";
 import menu from "./../../../public/assets/icon/menu.svg";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import {
+  Button,
+  Modal,
+  Box,
+  Typography,
+  Container,
+  Grid,
+  Snackbar
+} from "@mui/material";
+import { Formik } from "formik";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4
+};
 export default function Header(props) {
   const { handleShowMenu } = props;
+  const [open, setOpen] = useState(false);
+  const handleClose = () => setOpen(false);
   useEffect(() => {
     window.onscroll = function () {
       myFunction();
@@ -24,6 +48,9 @@ export default function Header(props) {
       }
     }
   }, []);
+  const handleLogin = () => {
+    setOpen(true);
+  };
   return (
     <div className={styles.header}>
       <div id="header-landing" className={styles.mainHeader}>
@@ -55,6 +82,11 @@ export default function Header(props) {
               <div className={styles.navItem}>
                 <Link href="/contact-us">Contact Us</Link>
               </div>
+              <div className={styles.navItem}>
+                <Button variant="contained" onClick={handleLogin}>
+                  Login
+                </Button>
+              </div>
             </div>
             <div
               onClick={handleShowMenu}
@@ -65,6 +97,142 @@ export default function Header(props) {
           </div>
         </div>
       </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Container
+            maxWidth="sm"
+            sx={{ height: "58vh", display: "flex", alignItems: "center" }}
+          >
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={12}>
+                <Typography>
+                  <Box fontSize={32}>Login</Box>
+                  <Box fontSize={12} fontWeight={400} lineHeight="16px">
+                    Welcome back!
+                  </Box>
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={12}>
+                <Formik
+                  initialValues={{ email: "", password: "" }}
+                  validate={(values) => {
+                    const errors = {};
+                    if (!values.email) {
+                      errors.email = "Required";
+                    } else if (
+                      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
+                        values.email
+                      )
+                    ) {
+                      errors.email = "Invalid email address";
+                    }
+                    return errors;
+                  }}
+                  onSubmit={(values, { setSubmitting }) => {
+                    setTimeout(() => {
+                      alert(JSON.stringify(values, null, 2));
+                      setSubmitting(false);
+                    }, 400);
+                  }}
+                >
+                  {({
+                    values,
+                    errors,
+                    touched,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                    isSubmitting
+                    /* and other goodies */
+                  }) => (
+                    <form
+                      sx={{ margin: 10, height: "40px" }}
+                      noValidate
+                      autoComplete="off"
+                      onSubmit={handleSubmit}
+                    >
+                      <Typography>
+                        <Box fontSize={16} fontWeight={700} lineHeight="24px">
+                          Email atau Username
+                        </Box>
+                      </Typography>
+                      <input
+                        type="text"
+                        onChange={handleChange}
+                        name="username"
+                        placeholder="contoh@versinema.com"
+                      />
+                      <Typography>
+                        <Box fontSize={16} fontWeight={700} lineHeight="24px">
+                          Password
+                        </Box>
+                      </Typography>
+                      <input
+                        type="password"
+                        onChange={handleChange}
+                        name="password"
+                        autoComplete="on"
+                        placeholder="Password kamu"
+                      />
+                      <div
+                        sx={{
+                          display: "flex",
+                          justifyContent: "flex-start",
+                          marginTop: 10
+                        }}
+                      >
+                        <a color="textPrimary" href="forgot-password" replace>
+                          <Typography
+                            variant="caption"
+                            display="block"
+                            gutterBottom
+                          >
+                            Forget Password ?
+                          </Typography>
+                        </a>
+                      </div>
+                      <div
+                        sx={{
+                          display: "flex",
+                          justifyContent: "flex-start",
+                          marginTop: 10
+                        }}
+                      >
+                        <Button type="submit">Masuk</Button>
+                      </div>
+                      <div
+                        sx={{
+                          display: "flex",
+                          justifyContent: "flex-start",
+                          marginTop: 10
+                        }}
+                      >
+                        <Link href="/register" passHref>
+                          <Button component="a">Register</Button>
+                        </Link>
+                      </div>
+                    </form>
+                  )}
+                </Formik>
+              </Grid>
+            </Grid>
+            {/* <Snackbar
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right"
+              }}
+              open={open}
+              autoHideDuration={6000}
+              message={this.props.userStore.pesanError}
+            /> */}
+          </Container>
+        </Box>
+      </Modal>
     </div>
   );
 }
