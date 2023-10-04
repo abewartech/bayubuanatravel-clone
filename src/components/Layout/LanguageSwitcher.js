fd76cecbca7c1e61f9c12f3cd30d377137ec910b
ABE <abeabeabeabe444@gmail.com>
2023-10-04 12:30:20 +0700

    ✨ siwtcher language

---
+++ src/components/Layout/LanguageSwitcher.js
@@ -0,0 +1,71 @@
+import React from "react";
+import useTranslation from "next-translate/useTranslation";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { Button, Menu, MenuItem } from "@mui/material";
+
+const languages = ["en", "id"];
+
+function LanguageSwitcher() {
+  const { lang } = useTranslation();
+  const router = useRouter();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleOpenMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+
+  const changeLanguage = (newLang) => {
+    // Use the Next.js router to push a new route with the desired locale
+    router.push("/", "/", { locale: newLang });
+
+    // Close the menu
+    handleCloseMenu();
+  };
+
+  return (
+    <div>
+      <Button
+        aria-controls="language-menu"
+        aria-haspopup="true"
+        onClick={handleOpenMenu}
+        color="inherit"
+      >
+        <Image
+          src={`/assets/flags/${lang}.png`}
+          alt={lang}
+          width={24}
+          height={16}
+        />
+      </Button>
+      <Menu
+        id="language-menu"
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleCloseMenu}
+      >
+        {languages.map((language) => (
+          <MenuItem
+            key={language}
+            selected={lang === language}
+            onClick={() => changeLanguage(language)}
+          >
+            <Image
+              src={`/assets/flags/${language}.png`}
+              alt={language}
+              width={24}
+              height={16}
+            />
+            {language}
+          </MenuItem>
+        ))}
+      </Menu>
+    </div>
+  );
+}
+
+export default LanguageSwitcher;
