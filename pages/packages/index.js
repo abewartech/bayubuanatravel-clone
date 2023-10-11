1d50f0454d1ea149f78dea40d180ba604a69a694
ABE <abeabeabeabe444@gmail.com>
2023-10-11 14:55:13 +0700

    💄 api products

--- pages/packages/index.js
+++ pages/packages/index.js
@@ -1,21 +1,45 @@
-// import { useRouter } from "next/router";
-import Link from "next/link";
+import { useState, useEffect } from "react";
 import Layout from "../../src/components/Layout";
 import Card from "../../src/components/common/Card";
 import HeaderPage from "../../src/components/common/HeaderPage";
+import MuiAlert from "@mui/material/Alert";
 import resort from "./../../public/assets/resort.jpg";
+import API from "../../src/common/api";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 export default function TypeDestination() {
-  // const currUrl = useRouter();
-  // const typePage = currUrl.query.type;
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   const breadcrumb = [
     {
-      name: "Home",
+      name: "Home"
     },
     {
-      name: "Package",
-    },
+      name: "Package"
+    }
   ];
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await API.get("/products/v1/external?page=1&size=8");
+        setData(response.data); // Assuming the API response is an array
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setError(error?.message || "An error occurred");
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures the effect runs once on mount
+
   return (
     <Layout>
       <HeaderPage
@@ -25,13 +49,19 @@ export default function TypeDestination() {
       />
       <div className="container mb-5">
         <div className="row">
-          {[...Array(8)].map((item, idx) => {
-            return (
+          {loading ? (
+            <p>Loading...</p>
+          ) : error ? (
+            <Alert severity="error">{error}</Alert>
+          ) : data.length === 0 ? (
+            <p>No data found</p>
+          ) : (
+            data.map((item, idx) => (
               <div className={"col-lg-3 col-md-6 col-12"} key={idx}>
-                  <Card type="common" />
+                <Card type="common" data={item} />
               </div>
-            );
-          })}
+            ))
+          )}
         </div>
       </div>
     </Layout>
