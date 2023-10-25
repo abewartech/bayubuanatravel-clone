7e25719e254cbee80e0568f604fe35e0a0d02aa2
ABE <abeabeabeabe444@gmail.com>
2023-10-25 10:34:20 +0700

    💄 dummy history

--- src/components/Profile/History.js
+++ src/components/Profile/History.js
@@ -5,6 +5,7 @@ import { Button } from "@mui/material";
 import { useState, useEffect } from "react";
 import axios from "axios"; // Import Axios
 import API from "../../common/api";
+import calendar from "./calendar.svg";
 
 export default function History(props) {
   const router = useRouter();
@@ -15,9 +16,7 @@ export default function History(props) {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await API.get(
-          "/orders/v1/client"
-        );
+        const response = await API.get("/orders/v1/client");
         setData(response.data);
       } catch (error) {
         console.error("Error fetching data: ", error);
@@ -30,6 +29,11 @@ export default function History(props) {
   const handleFilter = (status) => {
     setStatusTrx(status);
   };
+
+  const print = () => {
+
+  }
+  
   return (
     <div className="col-lg-8">
       <div className={styles.menuShow}>
@@ -102,6 +106,66 @@ export default function History(props) {
           ) : (
             <div>No Transaction History</div>
           )}
+          <div className={styles.historyItem}>
+            <div className={styles.historyDate}>
+              <span>
+                <Image src={calendar} alt="calendar" />
+              </span>
+              12 Juli 2023 23:00
+            </div>
+            <div className={styles.historyContainer}>
+              <div className={styles.historyLeft}>
+                <div className={styles.historyImg}></div>
+                <div className={styles.historyWrap}>
+                  <div className={styles.historyStatus}>Not Paid</div>
+                  <div className={styles.historyName}>Paket Umroh 1</div>
+                  {/* <div className={styles.history}></div> */}
+                </div>
+              </div>
+              <div className={styles.historyRight}>
+                <div className={styles.historyLabel}>Total Belanja</div>
+                <div className={styles.historyPrice}>Rp 7.000.000</div>
+              </div>
+            </div>
+            <div className={styles.historyAction}>
+              <div className={styles.historyDetail}>
+                <Button>Detail Paket</Button>
+              </div>
+              <div className={styles.historySee}>
+                <Button>Bayar</Button>
+              </div>
+            </div>
+          </div>
+          <div className={styles.historyItem}>
+            <div className={styles.historyDate}>
+              <span>
+                <Image src={calendar} alt="calendar" />
+              </span>
+              12 Juli 2023 23:00
+            </div>
+            <div className={styles.historyContainer}>
+              <div className={styles.historyLeft}>
+                <div className={styles.historyImg}></div>
+                <div className={styles.historyWrap}>
+                  <div className={styles.historyStatusPaid}>Paid</div>
+                  <div className={styles.historyName}>Paket Umroh 1</div>
+                  {/* <div className={styles.history}></div> */}
+                </div>
+              </div>
+              <div className={styles.historyRight}>
+                <div className={styles.historyLabel}>Total Belanja</div>
+                <div className={styles.historyPrice}>Rp 7.000.000</div>
+              </div>
+            </div>
+            <div className={styles.historyAction}>
+              <div className={styles.historyDetail}>
+                <Button>Detail Paket</Button>
+              </div>
+              <div className={styles.historySee}>
+                <Button variant="outlined" color="success" onClick={print}>Print</Button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
