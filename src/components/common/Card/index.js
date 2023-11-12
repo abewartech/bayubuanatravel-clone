f71c69e2da3cbd188aad6dba6514d987649a0982
TIO <you@example.com>
2023-11-12 13:25:16 +0700

    🔥

--- src/components/common/Card/index.js
+++ src/components/common/Card/index.js
@@ -28,7 +28,8 @@ export default function Card(props) {
           (durationInMilliseconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
         );
 
-        return `${days} days ${hours} hours`;
+        return `${days} ${t('days')} ${hours} ${t('hours')}`;
+
       }
 
       return "N/A"; // Handle the case where data is missing or invalid
