db4e6957f980b4edaa105ac41ba388e287a57012
ABE <abeabeabeabe444@gmail.com>
2023-12-16 15:25:02 +0700

    🐛

--- pages/profile.js
+++ pages/profile.js
@@ -4,7 +4,7 @@ import HeaderPage from "../src/components/common/HeaderPage";
 import resort from "./../public/assets/resort.jpg";
 import NavProfile from "../src/components/Profile/NavProfile";
 import styles from "../src/components/Profile/Profile.module.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PrivateInformation from "../src/components/Profile/PrivateInformation";
 import History from "../src/components/Profile/History";
 import PasswordChange from "../src/components/Profile/PasswordChange";
@@ -12,6 +12,7 @@ import PasswordChange from "../src/components/Profile/PasswordChange";
 export default function Profile() {
   const router = useRouter();
   const [currMenu, setCurrMenu] = useState("personal");
+  const [scrollPosition, setScrollPosition] = useState(0);
   const breadcrumb = [
     {
       name: "Home"
@@ -21,6 +22,7 @@ export default function Profile() {
     }
   ];
   const handleNavigateMenu = (menu) => {
+    setScrollPosition(window.scrollY);
     setCurrMenu(menu);
     router.push(`?menu=${menu}`);
   };
@@ -32,6 +34,22 @@ export default function Profile() {
     gender: "",
     password: ""
   };
+
+  useEffect(() => {
+    // Event listener to handle scroll position after route change
+    const handleRouteChange = () => {
+      // Restore the scroll position
+      window.scrollTo(0, scrollPosition);
+    };
+
+    // Add the event listener
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // Remove the event listener on component unmount
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events, scrollPosition]);
 
   return (
     <Layout>
