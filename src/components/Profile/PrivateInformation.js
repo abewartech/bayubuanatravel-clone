a70a04493ce2735901fe2c04b4bda3f001ef7a73
ABE <abeabeabeabe444@gmail.com>
2024-02-28 22:17:54 +0700

    🐛

--- src/components/Profile/PrivateInformation.js
+++ src/components/Profile/PrivateInformation.js
@@ -128,6 +128,7 @@ export default function PrivateInformation(props) {
                     "Personal Information Has Been Successfully Updated"
                   );
                   setSnackbarOpen(true);
+                  location.reload()
                 }
               })
               .catch((err) => {
